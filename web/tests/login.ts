3181ccb6df326c61938b9c1911952356fd7191d0
skarmn <>
2022-01-27 17:57:19 +0200

    password validation

--- web/tests/login.ts
+++ web/tests/login.ts
@@ -1,11 +1,8 @@
 import * as DefaultPage from "../pageObjects/default.page"
 import * as LoginPage from "../pageObjects/login.page"
 import * as LoginWithEmail from "../pageObjects/loginWithEmail.page"
-
-const password = "123456"
-
-const incorrectEmail = "labas"
-const unregisteredEmail = "skaiste@gmail.com"
+import { myCredentials } from "../util/credentials"
+import * as credentials from "../util/incorrectCredentials"
 
 const mainUrl = ''
 const signInUrl = 'sign-in/'
@@ -14,6 +11,7 @@ describe('My Login application', () => {
 
     it('should open sign in with email page', async () => {
         DefaultPage.openUrl(mainUrl);
+        await browser.maximizeWindow();
         LoginPage.clickSignInWithEmailButton()
         browser.pause(3000)
 
@@ -30,17 +28,62 @@ describe('My Login application', () => {
 
     })
 
+    //email validation
+
     it('should not login with invalid email', async () => {
-        await LoginWithEmail.typeCredentials(incorrectEmail, password)
+        for (const invalidEmail of credentials.incorrectEmails) {
+        await LoginWithEmail.typeCredentials(invalidEmail, credentials.password)
+        await LoginWithEmail.clickSignInButton()
+        await browser.pause(3000)
+        await browser.refresh()  
+        }
+        
+    })
+
+    it('should not login with unregistered email', async () => {
+        await LoginWithEmail.typeCredentials(credentials.unregisteredEmail, credentials.password)
         await LoginWithEmail.clickSignInButton()
         await browser.pause(3000)
         
     })
 
-    it('should not login with unregistered email', async () => {
-        await LoginWithEmail.typeCredentials(unregisteredEmail, password)
+    //password validation
+
+    it('should not login with incorrect password', async () => {
+        await LoginWithEmail.typeCredentials(myCredentials.email, credentials.password)
         await LoginWithEmail.clickSignInButton()
         await browser.pause(3000)
         
     })
+
+    it('should not login with password which has less than 6 symbols', async () => {
+        await LoginWithEmail.typeCredentials(myCredentials.email, credentials.shortPass)
+        await LoginWithEmail.clickSignInButton()
+        await browser.pause(3000)
+        
+    })
+
+    it('should not login with password which has more than 100 symbols', async () => {
+        await LoginWithEmail.typeCredentials(myCredentials.email, credentials.longPass)
+        await LoginWithEmail.clickSignInButton()
+        await browser.pause(3000)
+        
+    })
+
+    it('should login with password which has 100 symbols', async () => {
+        await LoginWithEmail.typeCredentials(myCredentials.email, credentials.correctPass)
+        await LoginWithEmail.clickSignInButton()
+        await browser.pause(3000)
+        
+    })
+
+    //successful login
+
+    it('should login with registered email', async () => {
+        await LoginWithEmail.typeCredentials(myCredentials.email, myCredentials.password)
+        await LoginWithEmail.clickSignInButton()
+        await browser.pause(3000)
+        
+    })
+    
 });
