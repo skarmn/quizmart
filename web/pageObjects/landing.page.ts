87b1455f2012e977d6f3ce56c9262644520908bf
skarmn <>
2022-02-17 19:40:14 +0200

    wait and error messages

--- web/pageObjects/landing.page.ts
+++ web/pageObjects/landing.page.ts
@@ -5,3 +5,7 @@ const signInEmailButton = "//button[text()='Sign in with email']"
 export async function clickSignInWithEmailButton(): Promise<void> {
     await defaultPage.clickByLocator(signInEmailButton)
 }
+
+export async function waitUntilBtnSignInIsVisible():Promise<void>{
+    return await defaultPage.waitUntilElementIsVisibleInViewportByLocator(signInEmailButton)
+}
--- web/pageObjects/loginWithEmail.page.ts
+++ web/pageObjects/loginWithEmail.page.ts
@@ -1,10 +1,15 @@
 import * as defaultPage from "./default.page"
 
+const mainPageHeaders = '//main //h1'
 const signInButton = "//button[text()='Sign in']"
-
 const emailField = '//input[@placeholder = "Email"]'
-
 const passwordField = '//input[@type = "password"]'
+
+const formValidationMessage = '(//form //h3)[1]'
+const emailValidationMessage = '//div[./input[@name="email"]]//h3'
+const passwordValidationMessage = '//div[./input[@name="password"]]//h3'
+const createAccountLink = '//a[@href="/sign-up"]'
+const forgotPasswordLink = '//a[@href="/reset-password"]'
 
 export async function clickSignInButton(): Promise<void> {
     await defaultPage.clickByLocator(signInButton)
@@ -15,3 +20,24 @@ export async function typeCredentials(email:string, password:string): Promise<vo
     await defaultPage.typeTextByLocator(password, passwordField)
 }
 
+export async function waitUntilBtnSignInIsVisible():Promise<void>{
+    return await defaultPage.waitUntilElementIsVisibleInViewportByLocator(signInButton)
+}
+
+//-------------------------------------GETS-----------------------------------------//
+
+export async function getMainPageHeaderText(): Promise<string> {
+    return await defaultPage.getElementTextByLocator(mainPageHeaders)
+}
+
+export async function getInvalidCredentialsErrorMessage(): Promise<string> {
+    return await defaultPage.getElementTextByLocator(formValidationMessage)
+}
+
+export async function getEmailValidationErrorMessage(): Promise<string> {
+    return await defaultPage.getElementTextByLocator(emailValidationMessage)
+}
+
+export async function getPasswordValidationErrorMessage(): Promise<string> {
+    return await defaultPage.getElementTextByLocator(passwordValidationMessage)
+}
